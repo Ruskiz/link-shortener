6ee077a7a288e0023fe501d3d1566bfb6d163548
Radkevich Liudmila <gamaleva@tut.by>
2018-06-14 16:06:35 +0300

    Add validation form

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { RegistComponent } from './components/regist/regist.component';
@@ -12,13 +15,15 @@ import { LoginComponent } from './components/login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
-	RegistComponent,
+  RegistComponent,
   LoginComponent
-	
-	
+  
+  
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+	ReactiveFormsModule,
       RouterModule.forRoot([
          {
             path: 'regist',
--- front-end/src/app/components/login/login.component.ts
+++ front-end/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
@@ -8,8 +9,48 @@ import { Component, OnInit} from '@angular/core';
   moduleId: module.id   
 })
 export class LoginComponent implements OnInit {
+	myFirstReactiveForm: FormGroup;
 	
-   constructor() { }
-   ngOnInit() {}
+   constructor(private fb: FormBuilder) { }
+   ngOnInit() {
+	this.initForm();
+   }
+
+   onSubmit() {
+const controls = this.myFirstReactiveForm.controls;
+
+ /** Проверяем форму на валидность */ 
+ if (this.myFirstReactiveForm.invalid) {
+  /** Если форма не валидна, то помечаем все контролы как touched*/
+  Object.keys(controls)
+   .forEach(controlName => controls[controlName].markAsTouched());
+   
+   /** Прерываем выполнение метода*/
+   return;
+  }
+
+ /** TODO: Обработка данных формы */
+ console.log(this.myFirstReactiveForm.value);
+}
+
+   isControlInvalid(controlName: string): boolean {
+	const control = this.myFirstReactiveForm.controls[controlName];
+ 	const result = control.invalid && control.touched;
+ 	return result;
+   }
+
+   /** Инициализация формы*/
+private initForm() {
+    this.myFirstReactiveForm = this.fb.group({
+      email: ['', [
+        Validators.required, Validators.email
+      ]
+      ],
+      password: ['', [
+        Validators.required
+      ]
+      ]
+    });
+  }
 	
 }
